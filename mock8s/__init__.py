cc997ad31572403ea20773e766dff6d36c3e46f3
Joao Pedro Portela <jportela@priogen.nl>
2020-06-20 18:11:37 +0200

    Mocking kube_config

--- mock8s/__init__.py
+++ mock8s/__init__.py
@@ -1,8 +1,10 @@
 from mock import patch
 from mock8s.client.api.mock_core_v1_api import MockCoreV1Api
-from mock8s.config.mock_kube_config import MockConfig
+from mock8s.config.mock_kube_config import load_kube_config
 
 
 def mock8s(function):
-    with patch('kubernetes.client.CoreV1Api', new=MockCoreV1Api):
+    with patch('kubernetes.client.CoreV1Api', new=MockCoreV1Api), \
+         patch('kubernetes.config.kube_config.load_kube_config',
+               new=load_kube_config):
         return function()
--- mock8s/config/mock_kube_config.py
+++ mock8s/config/mock_kube_config.py
@@ -1,24 +1,9 @@
 import os
+from kubernetes.config.kube_config import load_kube_config
 from kubernetes.config.config_exception import ConfigException
 
 
-class MockConfig:
-
-    @staticmethod
-    def load_kube_config(config_file=None, context=None,
-                         client_configuration=None,
-                         persist_config=True):
-        pass
-
-    @staticmethod
-    def list_kube_config_contexts(config_file=None):
-        pass
-
-    @staticmethod
-    def new_client_from_config(config_file=None, context=None,
-                               persist_config=True):
-        pass
-
-    @staticmethod
-    def incluster_config():
-        pass
+def load_kube_config(config_file=None, context=None,
+                     client_configuration=None,
+                     persist_config=True):
+    print("A")
