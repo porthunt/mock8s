5e10e928fbc9b6d74ee04a252a9eb8ffe0886ff9
Joao Pedro Portela <jportela.gomez@gmail.com>
2020-07-12 22:42:12 +0200

    Adding setup.py, updating README

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mock8s",
+    version="0.1.0",
+    author="porthunt",
+    author_email="porthunt@pm.me",
+    description="mock8s is an easy way to test your kubernetes resources.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/porthunt/mock8s",
+    packages=setuptools.find_packages(),
+    license="Apache",
+    test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.6',
+)
